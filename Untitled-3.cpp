6b2c3d82e3611d4202a034e0d1f02cbab8d4886e
lotor x <160056541+mahmoud325124322121@users.noreply.github.com>
2025-01-29 21:39:35 +0200

    Add files via upload

---
+++ Untitled-3.cpp
@@ -0,0 +1,29 @@
+    //uydy   "?
+  /* vykfvjoiyfufgy*/
+  
+  #include <iostream>
+using namespace std;
+  
+  int main()
+  {  cout <<"\n===================\n";
+    int price =100; // new value 
+    price =200; //updata value 
+    cout << price;
+      cout <<"\n===================\n";
+    int mA7shey = 244;
+    cout <<mA7shey;
+    cout <<"\n===================\n";
+    
+    return 0;
+  }
+ 
+
+
+
+
+
+
+
+
+
+
